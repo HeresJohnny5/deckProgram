c3e57117cba5e488ce28816ebbbf944b8e2d930c
John Erickson <ericksonjohn08@hotmail.com>
2018-07-07 15:22:47 -0400

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+class Deck {
+  List<Card> cards; // this is type List which is an array in Dart and should contain only instances of Card
+
+  Deck() {
+    
+  }
+}
+
+class Card {
+  String suit;
+  String rank;  
+}
