3fd09bdf6972a83548fc8a8cdcf61b046ff86c81
John Erickson <ericksonjohn08@hotmail.com>
2018-07-09 12:43:25 -0400

    Add loop logic to class Deck.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
+void main() {
+  new Deck();
+}
+
 class Deck {
-  List<Card> cards; // this is type List which is an array in Dart and should contain only instances of Card
+  List<Card> cards = []; // this is type List which is an array in Dart and should contain only instances of Card
 
   Deck() {
-    
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(suit, rank);
+        cards.add(card);
+      }
+    }
   }
 }
 
 class Card {
   String suit;
   String rank;  
+
+  Card(this.suit, this.rank); // function constructor shorthand
 }
